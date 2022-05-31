fd68f946953daf8a6b4441f375675b7d8ed5b26d
ttk_fuzztaro <nob.third.stone.from.the.sun@gmail.com>
2022-05-31 19:08:01 +0900

    add os usage

--- tool_box_001/date_str.py
+++ tool_box_001/date_str.py
@@ -8,6 +8,7 @@ print(current_date_as_date)
 current_date_as_str = current_date_as_date.strftime('%Y%m%d')
 print(current_date_as_str)
 
+# strings to date
 input_str = '20220401'
 parsed_str_as_date = datetime.datetime.strptime(input_str,'%Y%m%d')
 print(parsed_str_as_date)
---
+++ tool_box_001/os_get_file_names.py
@@ -0,0 +1,3 @@
+import os
+print(os.path.basename(__file__))
+# os_get_file_names.py
