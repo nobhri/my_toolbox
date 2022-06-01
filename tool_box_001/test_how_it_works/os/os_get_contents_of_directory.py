dd5260f972e2614002d6cf0882867f116cff638d
nobhri <nob.third.stone.from.the.sun@gmail.com>
2022-06-01 19:30:33 +0900

    add usage of os.path.join()

--- tool_box_001/test_how_it_works/os/os_get_contents_of_directory.py
+++ tool_box_001/test_how_it_works/os/os_get_contents_of_directory.py
@@ -1,6 +1,6 @@
 import os
 
-dir_path = './tool_box_001/'
+dir_path = os.getcwd()
 
 # get directory contents as a list
 list_of_the_file_name_of_the_directory = os.listdir(dir_path)
@@ -11,6 +11,6 @@ print(list_of_the_file_name_of_the_directory)
 list_of_just_files = []
 for file_or_directory_name in list_of_the_file_name_of_the_directory:
     # print(os.path.isfile(dir_path + file_or_directory_name))#return true if it is a file
-    if os.path.isfile(dir_path + file_or_directory_name):
+    if os.path.isfile(file_or_directory_name):
         list_of_just_files.append(file_or_directory_name)
 print(list_of_just_files)
