643afbf10c327581f9ddf34af2fc3a032559599e
nobhri <nob.third.stone.from.the.sun@gmail.com>
2022-06-01 20:29:08 +0900

    pandas plot

--- tool_box_001/test_how_it_works/pandas/pandas_plot.py
+++ tool_box_001/test_how_it_works/pandas/pandas_plot.py
@@ -1,5 +1,10 @@
 import pandas as pd
+import matplotlib.pyplot as plt
 
 df = pd.read_csv('test.csv')
 # print(df.head())
+
+plt.figure()# initialize plt
 df.plot()
+plt.show()
+plt.savefig('test.png')# save as .png image file
