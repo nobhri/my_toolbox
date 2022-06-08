11553d0330927150659e4cf8b5986525e06522ab
nobhri <nob.third.stone.from.the.sun@gmail.com>
2022-06-08 20:03:52 +0900

    add some basic c language script

---
+++ tool_box_002/addition_02.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+void main(){
+    int val_1 = 1;
+    int val_2 = 2;
+    int val_3 = val_1 + val_2;
+    printf("%d\n",val_3);
+}
---
+++ tool_box_002/if_01.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+void main(){
+    int val_1 = 1;
+    int val_2 = 2;
+    if(val_1 < val_2)
+        printf("val_2 is bigger\n");
+}
