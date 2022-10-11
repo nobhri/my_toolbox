fc99037ed30690d7c86e897768b13c2cfb59131a
nobhri <nob.third.stone.from.the.sun@gmail.com>
2022-10-11 20:10:42 +0900

    update delete_tweet.py

--- 001_Python/what_may_be_useful/delete_tweet.py
+++ 001_Python/what_may_be_useful/delete_tweet.py
@@ -5,6 +5,8 @@ import datetime
 from dateutil import tz
 import tweepy
 
+print('set user id (after "@")')
+user_screen_name = input()
 
 print('set maximum YYYYMMDD')
 target_ymd  = input()
@@ -15,7 +17,7 @@ target_ymd = datetime.datetime(
                 tzinfo = tz.gettz('UTC'))# Timezone
 
 
-print(f'Tweets until {str(target_ymd)} is going to dalete.')
+print(f'Tweets until {str(target_ymd)} is going to delete.')
 
 consumer_key = os.environ['CONSUMER_KEY']
 consumer_secret = os.environ['CONSUMER_SECRET']
@@ -29,9 +31,11 @@ api = tweepy.API(auth)
 
 counter = 0
 for tweet_i in tweepy.Cursor(api.user_timeline).items():
-    if tweet_i.created_at <= target_ymd:
+    if (tweet_i.created_at <= target_ymd) & (tweet_i.user.screen_name == user_screen_name):
         print(tweet_i.created_at)
         print(tweet_i.text)
+        print(tweet_i.user.name)
+        print(tweet_i.user.screen_name)
         api.destroy_status(tweet_i.id)
         counter += 1
 
